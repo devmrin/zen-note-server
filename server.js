59c97c7082284794eef4f55adaf46f0cc8104e11
Mrinmay <devmrin@gmail.com>
2025-09-06 15:15:02 +0530

    fix: update server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ fastify.get('/', async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen({ port: process.env.PORT || 3000, host: '0.0.0.0' })
+    await fastify.listen(process.env.PORT || 3000, '0.0.0.0')
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
